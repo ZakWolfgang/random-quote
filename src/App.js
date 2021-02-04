a460939b8271f10024686de4fa8f3c297a11410b
zakwolfgang <zachary.shultz@selu.edu>
2021-02-04 13:56:54 -0600

    created styles.css and implemented twitter link (not working) and on button clicks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
+import './styles.css';
 
 const App = () => {
   const [quotes, setQuotes] = useState('');
+  
+ 
 
   const getQuote = () => {
     fetch ("https://type.fit/api/quotes")
@@ -15,13 +18,30 @@ const App = () => {
     
   }
 
+  useEffect(() => {
+    getQuote();
+
+  }, []);
 
   return (
     <div className="App">
-      {quotes.text}
-      <button onClick={getQuote}>Get Quote</button>
+      <div className="quote"> 
+        <p>{quotes.text}</p>
+        <p>Author: {quotes.author}</p>
+        <div className="btnContainer">
+          <button onClick={getQuote} className="btn">Get quote</button>
+          <a 
+            href={'https://twitter.com/intent/tweet?text=${quotes.text}'} 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn"
+          >
+            Tweet
+          </a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
